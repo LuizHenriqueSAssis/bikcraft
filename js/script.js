810f442138c74fca3c8fb565e6298bfd2d14ec71
Luiz Henrique <luizhenriques.assis41@gmail.com>
2023-11-17 10:31:45 -0300

    PerguntasFrequentes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,57 @@
+//Ativar links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(link){
+function ativarLink(link) {
   const url = location.href;
   const href = link.href;
-if(url.includes(href)){
- link.classList.add('ativo')
-}
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
 }
 
 links.forEach(ativarLink);
+
+//Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+
+  console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
+
+//Perguntas Frequentes
+
+const Perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa);
+  pergunta.setAttribute("aria-expanded", ativa);
+  //console.log(resposta);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+Perguntas.forEach(eventosPerguntas);
+
+console.log(Perguntas);
+
+//Galeria de Bicicleta
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector("bicicleta-imagens");
+
+console.log(galeria, galeriaContainer);
