283562911778122d159e505167e543f20835e733
Luiz Henrique <luizhenriques.assis41@gmail.com>
2023-11-17 11:46:08 -0300

    galeriaImagensf

--- js/script.js
+++ js/script.js
@@ -52,6 +52,19 @@ console.log(Perguntas);
 
 //Galeria de Bicicleta
 const galeria = document.querySelectorAll(".bicicleta-imagens img");
-const galeriaContainer = document.querySelector("bicicleta-imagens");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
-console.log(galeria, galeriaContainer);
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  console.log(media);
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
